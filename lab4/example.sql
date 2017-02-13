310566f5231969cff96326ff5491c3b22ae72b1b
Ali Raza <aardz6@mail.missouri.edu>
2017-02-13 08:48:59 -0600

    Comments

--- lab4/example.sql
+++ lab4/example.sql
@@ -1,10 +1,16 @@
+############################################################## 
+#Capital and country have a one to one relationship.
+# One to one relationships are implemented by using the 
+# primary key of a table as a foreign key of another table 
+# and enforcing that each foreign key is unique.
+##############################################################
+
 DROP TABLE IF EXISTS `capital`;
 CREATE TABLE `capital`(
 	id int AUTO_INCREMENT,
 	`name` varchar(255),
 	PRIMARY KEY(id)
 );
-
 
 DROP TABLE IF EXISTS `country`;
 CREATE TABLE `country`(
