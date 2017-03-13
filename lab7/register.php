197ef2ad1475b758a47be263cb25ed1114fdf58e
Ali Raza <aardz6@mail.missouri.edu>
2017-03-13 10:41:47 -0500

    Register example

---
+++ lab7/register.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<head>
+  <title>Register</title>
+</head>
+
+<body>
+
+<form action="" method=POST>
+  Name:<br>
+  <input type=text name="name" required="required"> <br>
+  Pass:<br>
+  <input type="text" name="pass" required="required">
+  <br><br>
+  <input type="submit" name="submit">
+</form>
+
+<?php
+
+if(isset($_POST['submit'])){
+
+    include "../../CONFIG.php";
+
+    $mysqli = new mysqli($HOST, $USER, $PASS, $DB);
+
+    if($mysqli->connect_errno){
+        echo "Connection failed on line 5";
+        exit();
+    }
+
+    $query = "INSERT INTO user VALUES(?,?)";
+    $stmt = $mysqli->stmt_init();
+
+    if(!$stmt->prepare($query))
+    {
+        exit();
+    }
+
+    $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+    $stmt->bind_param("ss", $_POST['name'], $hash);
+
+    $stmt->execute();
+
+
+    /*$result = $stmt->get_result();
+
+    while ($row = $result->fetch_array(MYSQLI_NUM))
+    {
+        foreach ($row as $r)
+        {
+            echo "$r ";
+        }
+        echo "<br>";
+    }*/
+
+    $stmt->close();
+    $mysqli->close();
+
+}
+
+?>
