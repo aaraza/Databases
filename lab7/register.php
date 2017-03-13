bfe17308c262fb240281a40025b7b7f47c1cf580
Ali Raza <aardz6@mail.missouri.edu>
2017-03-13 11:28:10 -0500

    Updated register

--- lab7/register.php
+++ lab7/register.php
@@ -27,31 +27,46 @@ if(isset($_POST['submit'])){
         exit();
     }
 
-    $query = "INSERT INTO user VALUES(?,?)";
+    $query = "SELECT * FROM user WHERE user=?";
     $stmt = $mysqli->stmt_init();
-
     if(!$stmt->prepare($query))
     {
         exit();
     }
-
-    $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
-
-    $stmt->bind_param("ss", $_POST['name'], $hash);
-
+    $stmt->bind_param("s", $_POST['name']);
     $stmt->execute();
+    $result = $stmt->get_result();
+    $exists = $result->num_rows;
+    echo "Found: " . $exists;
 
-
-    /*$result = $stmt->get_result();
-
-    while ($row = $result->fetch_array(MYSQLI_NUM))
-    {
-        foreach ($row as $r)
+    /* If we wanted to print results:
+        while ($row = $result->fetch_array(MYSQLI_NUM))
         {
-            echo "$r ";
+            foreach ($row as $r)
+            {
+                echo "$r ";
+            }
+            echo "<br>";
         }
-        echo "<br>";
-    }*/
+    */
+
+    if($exists == 0){
+        $query = "INSERT INTO user VALUES(?,?)";
+        $stmt = $mysqli->stmt_init();
+
+        if(!$stmt->prepare($query)){
+            exit();
+        }
+
+        $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+        $stmt->bind_param("ss", $_POST['name'], $hash);
+
+        $stmt->execute();
+        echo "<hr>User created<br>";
+    } else {
+        echo "<hr>User name taken";
+    }
 
     $stmt->close();
     $mysqli->close();
