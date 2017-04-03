a04425a5562d378b7710586eab38146c293c0579
Ali Raza <aardz6@mail.missouri.edu>
2017-04-03 09:13:15 -0500

    Lab 9/10 Search

---
+++ lab7/working.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<head>
+  <title>Register</title>
+</head>
+
+<body>
+
+<form action="" method=POST>
+  Name:<br>
+  <input type=text name="name" required="required"> <br>
+  Pass:<br>
+  <input type="text" name="pass" required="required">
+  <br><br>
+  <input type="submit" name="submit">
+</form>
+
+<?php
+
+if(isset($_POST['submit'])){
+
+    include "../../CONFIG.php";
+
+    $mysqli = new mysqli($HOST, $USER, $PASS, $DB);
+
+    if($mysqli->connect_errno){
+        echo "Connection failed on line 5";
+        exit();
+    }
+
+    $query = "SELECT * FROM user WHERE user=?";
+    $stmt = $mysqli->stmt_init();
+    if(!$stmt->prepare($query))
+    {
+        exit();
+    }
+    $stmt->bind_param("s", $_POST['name']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $exists = $result->num_rows;
+    echo "Found: " . $exists;
+
+    /* If we wanted to print results:
+        while ($row = $result->fetch_array(MYSQLI_NUM))
+        {
+            foreach ($row as $r)
+            {
+                echo "$r ";
+            }
+            echo "<br>";
+        }
+    */
+
+    if($exists == 0){
+        $query = "INSERT INTO user VALUES(?,?)";
+        $stmt = $mysqli->stmt_init();
+
+        if(!$stmt->prepare($query)){
+            exit();
+        }
+
+        $hash = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+
+        $stmt->bind_param("ss", $_POST['name'], $hash);
+
+        $stmt->execute();
+        echo "<hr>User created<br>";
+    } else {
+        echo "<hr>User name taken<br>";
+    }
+
+    $stmt->close();
+    $mysqli->close();
+
+}
+
+?>
---
+++ lab9/index.php
@@ -0,0 +1,72 @@
+<?php
+/*
+ * @author Ali Raza
+ * @description: Execute multiple queries on one page using a dropdown.
+ */
+?>
+
+<html>
+    <head>
+        <!--  I USE BOOTSTRAP BECAUSE IT MAKES FORMATTING/LIFE EASIER -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"><!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"><!-- Optional theme -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script><!-- Latest compiled and minified JavaScript -->
+    </head>
+
+<body class="container">
+
+<br><br>
+<form action="" method="POST" class="col-md-4 col-md-offset-5">
+	Search: 
+	<input type="text" name="search">
+	<br><br>
+	<input type="radio" name="radios" value="0" checked="check"> First Name&nbsp
+	<input type="radio" name="radios" value="1"> Last Name
+	<br><br>
+	<input class="btn btn-primary" type="submit" name="submit" value="Execute">
+</form>
+
+<?php
+if(isset($_POST['submit'])){
+
+	include "../../CONFIG.php";
+	$mysqli = new mysqli($HOST, $USER, $PASS, $DB);
+
+	if ($mysqli->connect_errno) { //Terminate script if there is a connection error
+	    echo "Failed to connect to MySQLI on Line 5";
+	    exit();
+	}
+
+	/**
+	 * The big problem with my implementation of the search is that I am not using prepared statements.
+	 * For you lab, you will need to change this code to use prepared statements.
+	 */
+	if($_POST['radios'] == 0){
+		$sql = "SELECT * FROM person where `First Name` LIKE '" . $_POST['search'] . "%';";
+	} elseif ($_POST['radios'] == 1){
+		$sql = "SELECT * FROM person where `Last Name` LIKE '" . $_POST['search'] . "%';";
+	}
+
+	$result = $mysqli->query($sql); //Execute query
+    echo "<table class='table table-hover'>"; 
+    echo "Number of Results: " . $result->num_rows; //Display number of results
+    // Collect column names in a while loop and place mark them as headers in out table
+    while($fieldInfo = mysqli_fetch_field($result)){
+        echo "<th>". $fieldInfo->name. "</th>";
+    } 
+    echo "</thead>";
+    while($row = $result->fetch_array(MYSQLI_NUM)){ //Fetch the results as a numeric array
+        echo "<tr>"; //Each element of the array is a row
+        /*
+         * Each row's data is stored in an array
+         * Iterate that array and place each value
+         * into the table
+         */
+        foreach($row as $r){
+            echo "<td>" . $r . "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    $mysqli->close(); //Close mysql connection
+}
